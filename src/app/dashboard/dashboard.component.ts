465763ce5c666f72746416d3963ce944ac39b224
mrinal-kishore <kishore.mrinal1117@gmail.com>
2023-01-03 16:06:55 +0530

    Polar Area Chart via Chart.js

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  public chart: any;
+  constructor() {}
 
   ngOnInit(): void {
+    this.createChart();
   }
 
+  createChart() {
+    this.chart = new Chart('MyChart', {
+      type: 'polarArea',
+      data: {
+        // values on X-Axis
+        labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue'],
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: [11, 18, 7, 3, 14],
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(75, 192, 192)',
+              'rgb(255, 205, 86)',
+              'rgb(201, 203, 207)',
+              'rgb(54, 162, 235)',
+            ],
+          },
+        ],
+      },
+    });
+  }
 }
