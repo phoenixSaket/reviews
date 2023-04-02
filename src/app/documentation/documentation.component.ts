d485f80662ec62786ca26eec491a0f77af0eb96f
Saket Verma <saket3324verma@gmail.com>
2023-04-02 19:10:40 +0530

    commit all

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -7,18 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentationComponent implements OnInit {
   public features: any[] = [
-    {title: "Dashboard", text: "Pie & Bar charts for the ratings received by your apps"},
-    {title: "Add Apps", text: "Add any app from Google Play Store and Apple App Store"},
-    {title: "Word Clouds", text: "Generate word clouds for based on the reviews & sentiments"},
-    {title: "Monitor Apps", text: "Stay updated with the latest ratings and reviews for your added apps"},
-    {title: "Sort & Filter", text: "Sort and filter your apps with versions, years, ratings, keyword"},
-    {title: "Compare Apps", text: "Compare apps with the market and the competitors"},
-    {title: "New Apps Popup", text: "Get a popup with the reviews of your apps received while you were away"}
-  ]
+    { title: "Dashboard", text: "Pie & Bar charts for the ratings received by your apps" },
+    { title: "Add Apps", text: "Add any app from Google Play Store and Apple App Store" },
+    { title: "Word Clouds", text: "Generate word clouds for based on the reviews & sentiments" },
+    { title: "Monitor Apps", text: "Stay updated with the latest ratings and reviews for your added apps" },
+    { title: "Sort & Filter", text: "Sort and filter your apps with versions, years, ratings, keyword" },
+    { title: "Compare Apps", text: "Compare apps with the market and the competitors" },
+    { title: "New Apps Popup", text: "Get a popup with the reviews of your apps received while you were away" }
+  ];
+
+  public images: any[] = ["/assets/Images/1.jpg", "/assets/Images/2.jpg", "/assets/Images/3.jpg", "/assets/Images/4.jpg", "/assets/Images/5.jpg"];
+
+  public selectedImgIndex: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.rotator();
   }
 
+  rotator() {
+    let localthis = this;
+
+    function rotate() {
+      if (localthis.images.length - 1 > localthis.selectedImgIndex) {
+        localthis.selectedImgIndex += 1;
+      } else {
+        localthis.selectedImgIndex = 0;
+      }
+      localthis.rotator();
+    }
+
+    setTimeout(() => {
+      rotate();
+    }, 3000)
+  }
+
+  download(link: string) {
+    window.open(link, '_blank');
+  }
 }
