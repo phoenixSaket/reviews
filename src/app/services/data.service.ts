d03f38715179067184c42d218d314ba976a334bf
Saket Verma <saket3324verma@gmail.com>
2023-06-01 20:23:02 +0530

    deployment

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -32,7 +32,7 @@ export class DataService {
       if (values == 1) {
         x += 1;
         if (x == this.getTotalApps()) {
-          let date = new Date("03-01-2023").toString();
+          let date = new Date("05-20-2023").toString();
           localStorage.setItem("lastDate-reviews", date);
           let shouldShow: boolean = false;
           this.newAndroidReviews.forEach(el=> {
